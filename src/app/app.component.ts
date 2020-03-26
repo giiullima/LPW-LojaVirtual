c86d09e4d469b4e804051deaa98ad9abd881c05c
Giulliany Lima Bezerra <53629399+Giulliany@users.noreply.github.com>
2020-03-26 16:26:06 -0300

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { identifierModuleUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lojaVirtualCarrinho';
+  produtos = [{id:0, nome:'Arroz', valor:10.00}, {id:1, nome:'Feijão', valor:5.00}];
+  carrinho = [];
+  total = null;
+
+
+adicionar(produto){
+ var { nome, valor } = produto;
+
+  if (this.carrinho.indexOf(produto) === -1){
+    this.carrinho.push(produto);
+    produto.total=valor
+    produto.quantidade = 1
+  }
+  else{
+    produto.quantidade += 1;
+    produto.total = produto.quantidade * valor;
+  }
+  this.total += produto.valor;
+}
 }
